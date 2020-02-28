6834f67818d2cf1678137a71d8629f8920c287cd
Tom Ruette <tom.ruette@gmail.com>
2020-02-28 15:51:58 +0100

    now with a catchall

--- hlwtadmin/management/commands/resolve_venues.py
+++ hlwtadmin/management/commands/resolve_venues.py
@@ -26,44 +26,47 @@ class Command(BaseCommand):
 
         venues_without_org = Venue.objects.filter(organisation__isnull=True)
         for venue in venues_without_org:
-            name_prop, stad, land, bron, *rest = venue.raw_venue.split("|")
-            print(name_prop, stad, land)
-            ca = ConcertAnnouncement.objects.filter(raw_venue=venue).first()
-            lat = ca.latitude
-            lon = ca.longitude
-
             try:
-                loc = Location.objects.filter(id=location_pk[(stad, land)]).first()
-                print("found loc", loc)
-            except KeyError:
+                name_prop, stad, land, bron, *rest = venue.raw_venue.split("|")
+                print(name_prop, stad, land)
+                ca = ConcertAnnouncement.objects.filter(raw_venue=venue).first()
+                lat = ca.latitude
+                lon = ca.longitude
+
                 try:
-                    country = Country.objects.filter(id=country_pk[land]).first()
+                    loc = Location.objects.filter(id=location_pk[(stad, land)]).first()
+                    print("found loc", loc)
                 except KeyError:
-                    country = None
-                loc = Location.objects.create(
-                    verified=False,
-                    city=stad,
-                    country=country
-                )
-                loc.save()
-                location_pk[(stad, land)] = loc.id
-                print("created loc", loc)
+                    try:
+                        country = Country.objects.filter(id=country_pk[land]).first()
+                    except KeyError:
+                        country = None
+                    loc = Location.objects.create(
+                        verified=False,
+                        city=stad,
+                        country=country
+                    )
+                    loc.save()
+                    location_pk[(stad, land)] = loc.id
+                    print("created loc", loc)
 
-            try:
-                org = Organisation.objects.filter(id=organisation_pk[name_prop]).filter(location__isnull=True).first()
-                org.location = loc
-                print("added", loc, "to", org)
-            except (KeyError, AttributeError):
-                org = Organisation.objects.create(
-                    location=loc,
-                    verified=False,
-                    latitude=lat,
-                    longitude=lon,
-                    name=name_prop
-                )
-                org.save()
-                print("created org", org, org.location)
+                try:
+                    org = Organisation.objects.filter(id=organisation_pk[name_prop]).filter(location__isnull=True).first()
+                    org.location = loc
+                    print("added", loc, "to", org)
+                except (KeyError, AttributeError):
+                    org = Organisation.objects.create(
+                        location=loc,
+                        verified=False,
+                        latitude=lat,
+                        longitude=lon,
+                        name=name_prop
+                    )
+                    org.save()
+                    print("created org", org, org.location)
 
-            venue.organisation = org
-            venue.save()
-            print("venue is now", venue, venue.organisation)
+                venue.organisation = org
+                venue.save()
+                print("venue is now", venue, venue.organisation)
+            except ValueError:
+                pass
