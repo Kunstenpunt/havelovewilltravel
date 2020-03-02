fbd9d9862e1ff19c2eb7d2b18d83bb981c779275
Tom Ruette <tom.ruette@gmail.com>
2020-03-02 16:07:10 +0100

    get stuff ready to handle unprocessed announcements 2

--- hlwtadmin/models.py
+++ hlwtadmin/models.py
@@ -159,7 +159,10 @@ class ConcertAnnouncement(models.Model):
     def _create_new_unverified_organisation_and_relate_to_venue(self):
         name_prop, stad, land, bron = self.raw_venue.raw_venue.split("|")
         name = name_prop if len(name_prop.strip()) > 0 else self.raw_venue.raw_venue
-        loc = Location.objects.filter(city__istartswith=stad).first()
+        country = Country.objects.filter(name=land).first()
+        if not country:
+            country = Country.objects.filter(iso_code=land.lower()).first()
+        loc = Location.objects.filter(city__istartswith=stad).filter(country=country).first()
         org = Organisation.objects.create(name=name,
                                           annotation=(stad if len(stad.strip()) > 0 else "unknown city") + ", " + (land if len(land.strip()) else "unknown country") + " (" + bron + ")",
                                           location=loc, verified=False)
