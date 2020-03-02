d661cfe0e181dc2da6da9adc2b085af04ab0ed79
Tom Ruette <tom.ruette@gmail.com>
2020-03-02 14:51:05 +0100

    fixes #111

---
+++ hlwtadmin/management/commands/delete_concerts_with_too_many_announcements.py
@@ -0,0 +1,21 @@
+from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Location, Organisation, Country, Concert, RelationConcertConcert, RelationConcertOrganisation, RelationConcertArtist, Location
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Count
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        for concert in Concert.objects.annotate(num_ca=Count('concertannouncement')).filter(num_ca__gt=50):
+            print(concert.id, concert, concert.date)
+            RelationConcertOrganisation.objects.filter(concert=concert).delete()
+            RelationConcertArtist.objects.filter(concert=concert).delete()
+            RelationConcertConcert.objects.filter(concert_a=concert).delete()
+            RelationConcertConcert.objects.filter(concert_b=concert).delete()
+            for ca in ConcertAnnouncement.objects.filter(concert=concert):
+                ca.concert = None
+                ca.save()
+            concert.delete()
