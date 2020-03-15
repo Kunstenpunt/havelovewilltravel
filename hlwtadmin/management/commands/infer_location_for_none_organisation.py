019a49471a05a554225fc87c6b948b6d472bb76e
Tom Ruette <tom.ruette@gmail.com>
2020-03-15 15:17:18 +0100

    fixes #141

---
+++ hlwtadmin/management/commands/infer_location_for_none_organisation.py
@@ -0,0 +1,29 @@
+from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Location, Organisation, Country, Concert, RelationConcertConcert, RelationConcertOrganisation, RelationConcertArtist, Location
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Count
+
+from collections import Counter
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        for org in Organisation.objects.filter(location__country__isnull=True):
+            print(org, org.location)
+            locs = []
+            rels = RelationConcertOrganisation.objects.filter(organisation=org)
+            if len(rels) > 0:
+                for rel in rels:
+                    for rel2 in RelationConcertOrganisation.objects.filter(concert=rel.concert).exclude(organisation=org).exclude(organisation__isnull=True):
+                        org2 = rel2.organisation
+                        if org2.location != org.location:
+                            locs.append(org2.location)
+                if len(locs) > 0:
+                    new_loc = Counter(locs).most_common(1)[0][0]
+                    org.location = new_loc
+                    org.save()
+                    print(">>>", org, new_loc)
+            else:
+                org.delete()
