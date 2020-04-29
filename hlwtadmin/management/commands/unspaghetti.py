f2416a4b2286073914ebf32094999ff65b83c92a
Tom Ruette <tom.ruette@gmail.com>
2020-04-29 16:46:59 +0200

    more normalization

--- hlwtadmin/management/commands/unspaghetti.py
+++ hlwtadmin/management/commands/unspaghetti.py
@@ -9,15 +9,42 @@ from time import sleep
 
 from django.db.models import Avg, Count
 
+import pycountry
+
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
 
+    def norm(self, loc):
+
+        translate = {
+            "Nederland": "Netherlands",
+            "België": "Belgium",
+            "Duitsland": "Germany",
+            "UK": "United Kingdom",
+            "Antwerpen": "Antwerp",
+            "Gent": "Ghent",
+            "Wien": "Vienna",
+            "Wenen": "Vienna",
+            "Parijs": "Paris",
+            "Brussel": "Brussels"
+        }
+        country = loc.split("|")[-2]
+        country = translate[country] if country in translate else country
+        try:
+            country = pycountry.countries.get(name=country).alpha_2
+        except AttributeError:
+            pass
+        city = loc.split("|")[-3]
+        city = translate[city] if city in translate else city
+        return city, country
+
     def handle(self, *args, **options):
         count = 0
         for org in Organisation.objects.annotate(num_venues=Count('venue__raw_location', distinct=True)).filter(num_venues__gte=2):
-            venuelocs = set([", ".join(loc.split("|")[-3:-1]) for loc in org.venue_set.all().values_list('raw_location', flat=True)])
+            venuelocs = set([self.norm(loc) for loc in org.venue_set.all().values_list('raw_location', flat=True)])
             if len(venuelocs) > 1:
+                print(venuelocs)
                 count += 1
         print(count)
