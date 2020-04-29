6aa454b6731cd2be495987fd720da91c686638b7
Tom Ruette <tom.ruette@gmail.com>
2020-04-29 17:44:58 +0200

    let's count

--- hlwtadmin/management/commands/unspaghetti.py
+++ hlwtadmin/management/commands/unspaghetti.py
@@ -1,4 +1,4 @@
-from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Organisation
+from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Organisation, RelationConcertOrganisation
 
 from musicbrainzngs import set_useragent, search_artists, musicbrainz, get_artist_by_id
 from datetime import datetime
@@ -26,13 +26,23 @@ class Command(BaseCommand):
             "Nederland": "Netherlands",
             "België": "Belgium",
             "Duitsland": "Germany",
+            "Zwitserland": "CH",
             "UK": "United Kingdom",
             "Antwerpen": "Antwerp",
             "Gent": "Ghent",
             "Wien": "Vienna",
             "Wenen": "Vienna",
             "Parijs": "Paris",
-            "Brussel": "Brussels"
+            "Brussel": "Brussels",
+            "Bruxelles": "Brussels",
+            "Den Haag": "The Hague",
+            "München": "Munich",
+            "Den Bosch": "'s-Hertogenbosch",
+            "Brugge": "Bruges",
+            "United Kingdom Of Great Britain And Northern Ireland": "GB",
+            "The Netherlands": "NL",
+            "Londen": "London",
+            "Engeland": "GB",
         }
         country = loc.split("|")[-2]
         country = translate[country] if country in translate else country
@@ -40,7 +50,7 @@ class Command(BaseCommand):
             country = pycountry.countries.get(name=country).alpha_2
         except AttributeError:
             pass
-        city = loc.split("|")[-3]
+        city = loc.split("|")[-3].split(",")[0].replace("-", " ")
         city = translate[city] if city in translate else city
         return self.strip_accents(city.lower()), country
 
@@ -49,6 +59,14 @@ class Command(BaseCommand):
         for org in Organisation.objects.annotate(num_venues=Count('venue__raw_location', distinct=True)).filter(num_venues__gte=2):
             venuelocs = set([self.norm(loc) for loc in org.venue_set.all().values_list('raw_location', flat=True)])
             if len(venuelocs) > 1:
-                print(venuelocs)
-                count += 1
+                print(count + 1, org, org.pk)
+                # print("\t", venuelocs)
+                # print("\t\t", org.venue_set.all())
+                # split = input("Should I split this?")
+                # if split == "y":
+                #     RelationConcertOrganisation.objects.filter(organisation=org).delete()
+                #     for venue in org.venue_set.all():
+                #         venue.organisation = None
+                #         venue.save()
+                print("\n---\n\n")
         print(count)
