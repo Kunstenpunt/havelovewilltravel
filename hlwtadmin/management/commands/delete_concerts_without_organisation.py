11646376ac7b3f26f8966457d73a29afc44d14ad
Tom Ruette <tom.ruette@gmail.com>
2020-04-30 16:33:15 +0200

    clean empty concerts

---
+++ hlwtadmin/management/commands/delete_concerts_without_organisation.py
@@ -0,0 +1,18 @@
+from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Location, Organisation, Country, Concert, RelationConcertOrganisation, RelationConcertArtist
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        for concert in Concert.objects.filter(relationconcertorganisation__organisation__isnull=True):
+            print(concert, concert.pk)
+            for ca in ConcertAnnouncement.objects.filter(concert=concert):
+                ca.concert = None
+                ca.save()
+            for rel in RelationConcertArtist.objects.filter(concert=concert):
+                rel.delete()
+            concert.delete()
