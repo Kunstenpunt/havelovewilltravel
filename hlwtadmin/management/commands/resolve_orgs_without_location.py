b168b2d55ba730228c3769b15aeb06b07f3bfc3b
Tom Ruette <tom.ruette@gmail.com>
2020-05-04 20:53:33 +0200

    prepare a script to add locations to organisations

--- hlwtadmin/management/commands/resolve_orgs_without_location.py
+++ hlwtadmin/management/commands/resolve_orgs_without_location.py
@@ -2,32 +2,45 @@ from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncemen
 
 from django.core.management.base import BaseCommand, CommandError
 
+from pandas import read_excel
+from json import load
+from codecs import open
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
 
     def handle(self, *args, **options):
-        for organisation in Organisation.objects.filter(location__isnull=True):
+
+        with open("hlwtadmin/management/commands/cl.json", "r", "utf-8") as f:
+            cl = load(f)
+
+        for organisation in Organisation.objects.filter(location__isnull=True)[0:30]:
             venue = Venue.objects.filter(organisation=organisation).first()
             if venue:
                 print(venue)
                 org, stad, land, bron, *rest = venue.raw_venue.split("|")
-                print("location", stad, land)
-                country = Country.objects.filter(name=land).first()
-                if not country:
-                    country = Country.objects.filter(iso_code=land.lower()).first()
-                if country:
-                    print("found a country", country)
-                    location = Location.objects.filter(country=country).filter(city=stad).first()
-                    if location:
-                        print("found a location", location)
+                print("trying with", stad, land)
+                if venue.raw_location in cl:
+                    stad = cl[venue.raw_location]["clean city"]
+                    land = cl[venue.raw_location]["clean country"]
+                    print("wait a second, found something better", stad, land)
+
+                    print("hunting a location for", stad, land)
+                    country = Country.objects.filter(name=land).first()
+                    if not country:
+                        country = Country.objects.filter(iso_code=land.lower()).first()
+                    if country:
+                        print("found a country", country)
+                        location = Location.objects.filter(country=country).filter(city=stad).first()
+                        if location:
+                            print("found a location", location)
+                        else:
+                            print("creating a location with country", country)
+                            location = Location.objects.create(city=stad, country=country, verified=False)
                     else:
-                        print("creating a location with country", country)
-                        location = Location.objects.create(city=stad, country=country, verified=False)
-                else:
-                    print("did not find anything")
-                    location = Location.objects.create(city=stad + ", " + land, verified=False)
-                organisation.location = location
-                organisation.save()
-                print("linked", location, "to", organisation)
+                        print("did not find anything")
+                        location = Location.objects.create(city=stad + ", " + land, verified=False)
+                    organisation.location = location
+                    organisation.save()
+                    print("linked", location, "to", organisation)
