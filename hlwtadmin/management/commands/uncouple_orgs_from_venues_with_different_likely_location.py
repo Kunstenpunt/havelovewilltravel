0c97e43ab363ac7ddc68c115bd46cdafc37fcb4e
Tom Ruette <tom.ruette@gmail.com>
2020-05-29 14:00:40 +0200

    fixes #216

---
+++ hlwtadmin/management/commands/uncouple_orgs_from_venues_with_different_likely_location.py
@@ -0,0 +1,22 @@
+from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Location, Organisation, Country, Concert, RelationConcertOrganisation, RelationConcertArtist
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Count
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        for venue in Venue.objects.exclude(organisation__isnull=True).exclude(organisation__location__country__isnull=True):
+            print(venue, venue.pk)
+            org_loc = venue.organisation.location
+            likely_location = venue.concertannouncement_set.first().clean_location_from_string()
+            if likely_location:
+                print(org_loc, likely_location, org_loc == likely_location)
+                if org_loc != likely_location:
+                    print(org_loc.pk, org_loc, likely_location, likely_location.pk)
+                    input()
+                    venue.organisation = None
+                    venue.save(update_fields=["organisation"])
