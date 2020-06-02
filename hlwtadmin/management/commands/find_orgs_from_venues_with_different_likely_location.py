dee437b321252bfbbb5acac1d4b51daaa33b83ab
Tom Ruette <tom.ruette@gmail.com>
2020-06-02 15:26:51 +0200

    catch errors

--- hlwtadmin/management/commands/find_orgs_from_venues_with_different_likely_location.py
+++ hlwtadmin/management/commands/find_orgs_from_venues_with_different_likely_location.py
@@ -11,11 +11,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for venue in Venue.objects.exclude(organisation__isnull=True).exclude(organisation__location__country__isnull=True).exclude(non_assignable=True):
             org_loc = venue.organisation.location
-            if venue.concertannouncement_set.first():
-                likely_location = venue.concertannouncement_set.first().clean_location_from_string()
-                if likely_location and likely_location.pk != 34789:
-                    if org_loc != likely_location:
-                        print("\t".join([venue.raw_venue, str(venue.pk),
-                                         venue.organisation.name, str(venue.organisation.id),
-                                         str(org_loc.city), str(org_loc.country.name), str(org_loc.pk),
-                                         str(likely_location.city), str(likely_location.country.name if likely_location.country else None), str(likely_location.pk)]))
+            try:
+                if venue.concertannouncement_set.first():
+                    likely_location = venue.concertannouncement_set.first().clean_location_from_string()
+                    if likely_location and likely_location.pk != 34789:
+                        if org_loc != likely_location:
+                            print("\t".join([venue.raw_venue, str(venue.pk),
+                                             venue.organisation.name, str(venue.organisation.id),
+                                             str(org_loc.city), str(org_loc.country.name), str(org_loc.pk),
+                                             str(likely_location.city), str(likely_location.country.name if likely_location.country else None), str(likely_location.pk)]))
+            except Exception as e:
+                pass
