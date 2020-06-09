860763c372bfc722463fdef383d791c84406f616
Tom Ruette <tom.ruette@gmail.com>
2020-06-09 08:39:14 +0200

    run again 3

--- hlwtadmin/management/commands/resolve_venues.py
+++ hlwtadmin/management/commands/resolve_venues.py
@@ -3,6 +3,8 @@ from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncemen
 from django.core.management.base import BaseCommand, CommandError
 from json import load
 from codecs import open
+from collections import Counter
+from re import sub
 
 
 class Command(BaseCommand):
@@ -10,6 +12,23 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
+        cl_temp = {}
+        for venue in Venue.objects.exclude(organisation__isnull=True):
+            key = venue.raw_location
+            clean_loc = venue.organisation.location
+            clean_city = clean_loc.city
+            clean_country = clean_loc.country.name if clean_loc.country else None
+            if key not in cl_temp:
+                cl_temp[key] = []
+            cl_temp[key].append((clean_city, clean_country))
+
+        cl = {}
+        for key in cl_temp:
+            hit = Counter(cl_temp[key]).most_common(1)[0][0]
+            cl[key] = {"clean_city": hit[0], "clean_country": hit[1]}
+
+        print(cl)
+
         location_pk = {}
         country_pk = {}
 
@@ -21,7 +40,7 @@ class Command(BaseCommand):
             location_pk[(city, country)] = loc.id
             country_pk[country] = loc.country.id if loc.country else None
 
-        venues_without_org = Venue.objects.filter(organisation__isnull=True).filter(non_assignable=False)
+        venues_without_org = Venue.objects.exclude(raw_venue__icontains="|none|none|").exclude(raw_venue__istartswith="unknown").filter(organisation__isnull=True).filter(non_assignable=False)#.filter(pk__gt=100000)
         for venue in venues_without_org:
             try:
                 name_prop, stad, land, bron, *rest = venue.raw_venue.split("|")
@@ -30,54 +49,59 @@ class Command(BaseCommand):
                 lat = ca.latitude
                 lon = ca.longitude
 
-                with open("hlwtadmin/management/commands/clocs.json", "r", "utf-8") as f:
-                    cl = load(f)
-
                 print("trying to find a better match with", venue.raw_location, venue.raw_location in cl)
                 if venue.raw_location in cl:
-                    stad = cl[venue.raw_location]["clean city"]
-                    land = cl[venue.raw_location]["clean country"]
+                    stad = cl[venue.raw_location]["clean_city"]
+                    land = cl[venue.raw_location]["clean_country"]
                     print("wait a found something better", stad, land)
 
                 try:
                     loc = Location.objects.filter(id=location_pk[(stad, land)]).first()
                     print("found loc", loc)
                 except KeyError:
+                    # try:
+                    #     country = Country.objects.filter(id=country_pk[land]).first()
+                    # except KeyError:
+                    #     country = None
+                    # loc = Location.objects.create(
+                    #     verified=False,
+                    #     city=stad,
+                    #     country=country
+                    # )
+                    # loc.save()
+                    # location_pk[(stad, land)] = loc.id
+                    # print("created loc", loc)
+                    loc = None
+                    print("no loc found")
+
+                if loc:
                     try:
-                        country = Country.objects.filter(id=country_pk[land]).first()
-                    except KeyError:
-                        country = None
-                    loc = Location.objects.create(
-                        verified=False,
-                        city=stad,
-                        country=country
-                    )
-                    loc.save()
-                    location_pk[(stad, land)] = loc.id
-                    print("created loc", loc)
+                        name_prop_clean = sub("\d\d\d\d$", "", name_prop).strip()
+                        print("original name", name_prop, "so cleaned this would be", name_prop_clean)
+                        org = Organisation.objects.filter(name=name_prop_clean).filter(location__pk=loc.pk).first()
+                        print("checking for org", name_prop_clean, "with location", loc, "and found", org)
+                    except (KeyError, AttributeError):
+                        org = None
 
-                try:
-                    org = Organisation.objects.filter(name=name_prop).filter(location__pk=loc.pk).first()
-                    print("checking for org", name_prop ,"with location", loc, "and found", org)
                     if org is None:
-                        org = Organisation.objects.filter(name=name_prop).filter(location__isnull=True).first()
-                        print("checking for an org without location, and found", org)
-                        org.location = loc
-                        print("added", loc, "to", org)
-                except (KeyError, AttributeError):
-                    print("no org found, making one")
-                    org = Organisation.objects.create(
-                        location=loc,
-                        verified=False,
-                        latitude=lat,
-                        longitude=lon,
-                        name=name_prop
-                    )
-                    org.save()
-                    print("created org", org, org.location)
+                        print("no org found, making one")
+                        org = Organisation.objects.create(
+                            location=loc,
+                            verified=False,
+                            latitude=lat,
+                            longitude=lon,
+                            name=name_prop_clean
+                        )
+                        print("about to save", org)
+                        input()
+                        org.save()
+                        print("created org", org, org.location)
 
-                venue.organisation = org
-                venue.save()
-                print("venue is now", venue, venue.organisation)
-            except ValueError:
+                    print("about to connect", venue, "with", org)
+                    input()
+                    venue.organisation = org
+                    venue.save()
+                    print("venue is now", venue, venue.organisation)
+
+            except (KeyError, ValueError):
                 pass
