9326a580e59ab39d9908879685c587d9edcf1140
Tom Ruette <tom.ruette@gmail.com>
2020-06-14 09:05:06 +0200

    reconsider the sort name filter

--- hlwtadmin/management/commands/provide_sort_name_for_orgs.py
+++ hlwtadmin/management/commands/provide_sort_name_for_orgs.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        for org in Organisation.objects.filter(sort_name__isnull=True):
+        for org in Organisation.objects.filter(sort_name=""):
             print(org.name)
             sort_name = org.name
             sort_name = sub("^\s", "", sort_name)
