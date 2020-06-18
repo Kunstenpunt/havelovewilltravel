b0863331fcb62f5dff7a4dc6da5a0ed566ea5d68
Tom Ruette <tom.ruette@gmail.com>
2020-06-18 17:12:30 +0200

    switch to postgres in dev environment

--- havelovewilltravel/settings.py
+++ havelovewilltravel/settings.py
@@ -87,10 +87,21 @@ WSGI_APPLICATION = 'havelovewilltravel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hlwtadmin',
+        'USER': 'hlwtadmin',
+        'PASSWORD': 'hlwtadmin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
