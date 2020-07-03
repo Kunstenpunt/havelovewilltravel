a5733b0b5e33922a93a1fc6bcdb74b348f9287ec
Tom Ruette <tom.ruette@gmail.com>
2020-07-03 10:57:14 +0200

    getting ready for the 60k

--- hlwtadmin/management/commands/handle_unprocessed_concertannouncements.py
+++ hlwtadmin/management/commands/handle_unprocessed_concertannouncements.py
@@ -8,6 +8,10 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        for concertannouncement in ConcertAnnouncement.objects.filter(concert__isnull=True).exclude(ignore=True).filter(pk__in=(288230, 288306)):
+        for concertannouncement in ConcertAnnouncement.objects.filter(concert__isnull=True).exclude(ignore=True):
+            print("concertannouncement http://hlwtadmin.herokuapp.com/hlwtadmin/concertannouncement/" + str(concertannouncement.pk))
+            input()
             ca2c = ConcertannouncementToConcert(concertannouncement)
             ca2c.automate()
+            concertannouncement.save()
+            input()
