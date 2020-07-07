4f3f90c8e2f8125ee117afe6796d0b6915f3b7af
Tom Ruette <tom.ruette@gmail.com>
2020-07-07 10:20:56 +0200

    fixes #308

--- hlwtadmin/management/commands/clean_raw_venues.py
+++ hlwtadmin/management/commands/clean_raw_venues.py
@@ -11,12 +11,7 @@ class Command(BaseCommand):
         done = []
         for raw_venue in Venue.objects.all():
             if raw_venue.pk not in done:
-                for dup_venue in Venue.objects.filter(raw_venue=raw_venue.raw_venue).filter(organisation=raw_venue.organisation).exclude(pk=raw_venue.pk):
-                    print(raw_venue.pk, raw_venue, "is equal to", dup_venue.pk, dup_venue)
-                    for ca in ConcertAnnouncement.objects.filter(raw_venue=dup_venue):
-                        print("\tadjusting", ca.pk, ca)
-                        ca.raw_venue = raw_venue
-                        ca.save(update_fields=['raw_venue'])
-                    dup_venue.organisation = None
+                for dup_venue in Venue.objects.filter(raw_venue=raw_venue.raw_venue).exclude(organisation=raw_venue.organisation).exclude(pk=raw_venue.pk):
+                    print("http://hlwtadmin.herokuapp.com/hlwtadmin/venue/" + str(raw_venue.pk), raw_venue, "is similar to", "http://hlwtadmin.herokuapp.com/hlwtadmin/venue/" + str(dup_venue.pk), dup_venue)
                     done.append(dup_venue.pk)
-                    dup_venue.delete()
+
