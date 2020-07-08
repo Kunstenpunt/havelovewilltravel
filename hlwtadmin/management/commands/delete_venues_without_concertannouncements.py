ace64c3d43f40b0258a1c63958730c25c28bc81e
Tom Ruette <tom.ruette@gmail.com>
2020-07-08 09:47:18 +0200

    delete venus without concertannouncements

---
+++ hlwtadmin/management/commands/delete_venues_without_concertannouncements.py
@@ -0,0 +1,12 @@
+from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Location, Organisation, Country, Concert, RelationConcertConcert, RelationConcertOrganisation, RelationConcertArtist, Location
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Count
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        Venue.objects.filter(concertannouncement__isnull=True).delete()
