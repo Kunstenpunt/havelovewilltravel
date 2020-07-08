6ef6d132a1277ce0613270f4e1c3b7e9d928e0f5
Tom Ruette <tom.ruette@gmail.com>
2020-07-08 14:16:10 +0200

    redo the list of raw venues with conflicting orgs

--- hlwtadmin/management/commands/clean_raw_venues.py
+++ hlwtadmin/management/commands/clean_raw_venues.py
@@ -9,9 +9,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         done = []
+        i = 1
         for raw_venue in Venue.objects.all():
             if raw_venue.pk not in done:
                 for dup_venue in Venue.objects.filter(raw_venue=raw_venue.raw_venue).exclude(organisation=raw_venue.organisation).exclude(pk=raw_venue.pk):
-                    print("http://hlwtadmin.herokuapp.com/hlwtadmin/venue/" + str(raw_venue.pk), raw_venue, "is similar to", "http://hlwtadmin.herokuapp.com/hlwtadmin/venue/" + str(dup_venue.pk), dup_venue)
+                    print(i, "http://hlwtadmin.herokuapp.com/hlwtadmin/venue/" + str(raw_venue.pk), str(raw_venue).encode('utf-8'), "is similar to", "http://hlwtadmin.herokuapp.com/hlwtadmin/venue/" + str(dup_venue.pk), str(dup_venue).encode('utf-8'))
                     done.append(dup_venue.pk)
+                    i += 1
 
