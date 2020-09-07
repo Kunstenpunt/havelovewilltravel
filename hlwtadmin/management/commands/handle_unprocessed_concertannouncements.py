5483371bd731ec84e4b9b80de4f426baf20ca9bb
Tom Ruette <tom.ruette@gmail.com>
2020-09-07 16:26:42 +0200

    do more unhandled CAs

--- hlwtadmin/management/commands/handle_unprocessed_concertannouncements.py
+++ hlwtadmin/management/commands/handle_unprocessed_concertannouncements.py
@@ -9,8 +9,8 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        #for concertannouncement in ConcertAnnouncement.objects.filter(concert__isnull=True).exclude(ignore=True).filter(date__lt=datetime(2018, 8, 31))[:1000]:
-        for concertannouncement in ConcertAnnouncement.objects.filter(concert__isnull=True).exclude(ignore=True).filter(raw_venue__organisation__location__pk=34789):
+        for concertannouncement in ConcertAnnouncement.objects.filter(concert__isnull=True).exclude(ignore=True).filter(date__lt=datetime(2019, 9, 1))[:3000]:
+        #for concertannouncement in ConcertAnnouncement.objects.filter(concert__isnull=True).exclude(ignore=True).filter(raw_venue__organisation__location__pk=34789):
             print("concertannouncement\thttp://hlwtadmin.herokuapp.com/hlwtadmin/concertannouncement/" + str(concertannouncement.pk) + "\trelated venue\thttp://hlwtadmin.herokuapp.com/hlwtadmin/venue/" + str(concertannouncement.raw_venue.pk) if concertannouncement.raw_venue else "---")
             ca2c = ConcertannouncementToConcert(concertannouncement)
             ca2c.automate()
