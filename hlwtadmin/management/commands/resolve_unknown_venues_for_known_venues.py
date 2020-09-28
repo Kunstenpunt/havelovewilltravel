80a2ce1d3d01e2d3f99d6282f07b7ac04a3b02f0
Tom Ruette <tom.ruette@gmail.com>
2020-09-28 16:35:22 +0200

    list unknown venues that we actually know 2

--- hlwtadmin/management/commands/resolve_unknown_venues_for_known_venues.py
+++ hlwtadmin/management/commands/resolve_unknown_venues_for_known_venues.py
@@ -11,11 +11,15 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
+        i = 1
         for organisation in Organisation.objects.filter(name__icontains="unknown"):
             for rel in RelationConcertOrganisation.objects.filter(organisation=organisation):
                 concert = rel.concert
                 for ca in concert.concertannouncements():
-                    raw_venue = ca.raw_venue
-                    venue_organisation = raw_venue.organisation
-                    line = ["concert", str(concert), str(concert.pk), "with organisation", str(organisation), str(organisation.pk), "has ca", str(ca), str(ca.pk), "with raw venue", str(raw_venue), str(raw_venue.pk), "which has organisation", str(venue_organisation), str(venue_organisation.pk) if venue_organisation else str(None)]
-                    print("\t".join(line))
+                    if "songkick" in ca.gigfinder.name and ca.is_festival:
+                        raw_venue = ca.raw_venue
+                        venue_organisation = raw_venue.organisation
+                        if venue_organisation and "unknown" not in venue_organisation.name.lower():
+                            line = [str(i), "concert", str(concert), str(concert.pk), "with organisation", str(organisation), str(organisation.pk), "has ca", str(ca), str(ca.pk), "with raw venue", str(raw_venue), str(raw_venue.pk), "which has organisation", str(venue_organisation), str(venue_organisation.pk) if venue_organisation else str(None)]
+                            print("\t".join(line))
+                            i += 1
