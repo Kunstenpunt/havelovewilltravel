bbcf011c84db54028719e8a839fb386df58e560a
Tom Ruette <tom.ruette@gmail.com>
2020-10-01 14:29:42 +0200

    we have a backend!

--- hlwtadmin/admin.py
+++ hlwtadmin/admin.py
@@ -7,50 +7,114 @@ from .models import Concert, ConcertAnnouncement, Artist, Organisation, Location
     RelationConcertConcert, RelationOrganisationIdentifier, ExternalIdentifier, ExternalIdentifierService, LocationsMerge, \
     RelationLocationLocationType, RelationLocationLocation
 
+
+# Define the admin class
+class ArtistAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+class ConcertAnnouncementAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ["artist", "concert", "raw_venue"]
+
+
+class VenueAdmin(SimpleHistoryAdmin):
+    search_fields = ["raw_venue"]
+    autocomplete_fields = ["organisation"]
+
+
+class ConcertAdmin(SimpleHistoryAdmin):
+    search_fields = ["title"]
+
+
+class OrganisationAdmin(SimpleHistoryAdmin):
+    search_fields = ["name"]
+    autocomplete_fields = ['location']
+
+
+class LocationAdmin(SimpleHistoryAdmin):
+    search_fields = ["city"]
+    autocomplete_fields = ["country"]
+
+
+class CountryAdmin(SimpleHistoryAdmin):
+    search_fields = ["name"]
+
+
+class ExternalIdentifierAdmin(SimpleHistoryAdmin):
+    search_fields = ["identifier"]
+
+
+class RelationConcertArtistAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ["artist", "concert"]
+
+
+class RelationConcertOrganisationAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ["concert", "organisation"]
+
+
+class RelationOrganisationOrganisationAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ["organisation_a", "organisation_b"]
+
+
+class RelationArtistArtistAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ["artist_a", "artist_b"]
+
+
+class RelationConcertConcertAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ["concert_a", "concert_b"]
+
+
+class RelationLocationLocationAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ["location_a", "location_b"]
+
+
+class RelationOrganisationIdentifierAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ["organisation", "identifier"]
+
+
 # Register your models here.
+admin.site.register(ConcertAnnouncement, ConcertAnnouncementAdmin)
 
-admin.site.register(ConcertAnnouncement, SimpleHistoryAdmin)
+admin.site.register(Artist, ArtistAdmin)
 
-admin.site.register(Artist)
+admin.site.register(Concert, ConcertAdmin)
 
-admin.site.register(Concert, SimpleHistoryAdmin)
-
-admin.site.register(Organisation)
+admin.site.register(Organisation, OrganisationAdmin)
 admin.site.register(OrganisationType)
 
-admin.site.register(Location, SimpleHistoryAdmin)
+admin.site.register(Location, LocationAdmin)
 
-admin.site.register(Country, SimpleHistoryAdmin)
+admin.site.register(Country, CountryAdmin)
 
-admin.site.register(Venue, SimpleHistoryAdmin)
+admin.site.register(Venue, VenueAdmin)
 
 admin.site.register(GigFinderUrl)
 admin.site.register(GigFinder, SimpleHistoryAdmin)
 
 admin.site.register(Genre, SimpleHistoryAdmin)
 
-admin.site.register(ExternalIdentifier, SimpleHistoryAdmin)
+admin.site.register(ExternalIdentifier, ExternalIdentifierAdmin)
 admin.site.register(ExternalIdentifierService, SimpleHistoryAdmin)
 
-admin.site.register(RelationConcertOrganisation, SimpleHistoryAdmin)
+admin.site.register(RelationConcertOrganisation, RelationConcertOrganisationAdmin)
 admin.site.register(RelationConcertOrganisationType, SimpleHistoryAdmin)
 
-admin.site.register(RelationConcertArtist, SimpleHistoryAdmin)
+admin.site.register(RelationConcertArtist, RelationConcertArtistAdmin)
 admin.site.register(RelationConcertArtistType, SimpleHistoryAdmin)
 
-admin.site.register(RelationOrganisationOrganisation, SimpleHistoryAdmin)
+admin.site.register(RelationOrganisationOrganisation, RelationOrganisationOrganisationAdmin)
 admin.site.register(RelationOrganisationOrganisationType, SimpleHistoryAdmin)
 
-admin.site.register(RelationArtistArtist, SimpleHistoryAdmin)
+admin.site.register(RelationArtistArtist, RelationArtistArtistAdmin)
 admin.site.register(RelationArtistArtistType, SimpleHistoryAdmin)
 
-admin.site.register(RelationConcertConcert, SimpleHistoryAdmin)
+admin.site.register(RelationConcertConcert, RelationConcertConcertAdmin)
 admin.site.register(RelationConcertConcertType, SimpleHistoryAdmin)
 
-admin.site.register(RelationLocationLocation, SimpleHistoryAdmin)
+admin.site.register(RelationLocationLocation, RelationLocationLocationAdmin)
 admin.site.register(RelationLocationLocationType, SimpleHistoryAdmin)
 
-admin.site.register(RelationOrganisationIdentifier, SimpleHistoryAdmin)
+admin.site.register(RelationOrganisationIdentifier, RelationOrganisationIdentifierAdmin)
 
 admin.site.register(OrganisationsMerge)
 admin.site.register(ConcertsMerge)
