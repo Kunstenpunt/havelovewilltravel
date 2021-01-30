2b85fb66aadf3b38409888c028c97363e2e81944
Tom Ruette <tom.ruette@gmail.com>
2021-01-30 11:38:09 +0100

    try to resolve concerts without organisation by checking if their raw_venues have a linked org

--- hlwtadmin/management/commands/resolve_concerts.py
+++ hlwtadmin/management/commands/resolve_concerts.py
@@ -9,12 +9,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for concert in Concert.objects.filter(relationconcertorganisation__organisation__isnull=True):
+            print(concert, concert.pk)
             ca = ConcertAnnouncement.objects.filter(concert=concert).first()
             if ca:
                 venue = ca.raw_venue
                 print(venue)
                 organisation = venue.organisation
-                rel = RelationConcertOrganisation.objects.filter(concert=concert).first()
-                rel.organisation = organisation
-                rel.save()
-                print("linked", organisation, "to", concert)
+                print(organisation)
+                if organisation:
+                    print("about to link", organisation, "to", concert)
+                    input()
+                    rel = RelationConcertOrganisation.objects.create(concert=concert, organisation=organisation)
+                    rel.save()
+                else:
+                    print("no organisation to link")
