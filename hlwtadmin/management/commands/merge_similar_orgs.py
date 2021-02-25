b019709e685a78b784f317fca59579b3ff3d7e89
Tom Ruette <tom.ruette@gmail.com>
2021-02-25 10:21:39 +0100

    merge identical orgs in same loc

--- hlwtadmin/management/commands/merge_similar_orgs.py
+++ hlwtadmin/management/commands/merge_similar_orgs.py
@@ -27,10 +27,11 @@ class Command(BaseCommand):
             "ĂŠ": "é"
         }
 
-        for rel in RelationLocationLocation.objects.all():
-            loc = Location.objects.get(pk=rel.location_a.pk)
-            superloc = Location.objects.get(pk=rel.location_b.pk)
-            print(loc, "part of", superloc)
+        # for rel in RelationLocationLocation.objects.all():
+        #     loc = Location.objects.get(pk=rel.location_a.pk)
+        #     superloc = Location.objects.get(pk=rel.location_b.pk)
+        #     print(loc, "part of", superloc)
+        for loc in Location.objects.all():
             for org in Organisation.objects.filter(location=loc).order_by('disambiguation'):
                 for map in mapper:
                     new_name = str(org.name).replace(map, mapper[map]).strip()
@@ -38,7 +39,8 @@ class Command(BaseCommand):
                         org.name = new_name
                         org.save(update_fields=['name'])
                 mergeorgs = []
-                for simorg in Organisation.objects.filter(location=superloc).filter(name__unaccent__iexact=org.name).exclude(pk=org.pk):
+                #for simorg in Organisation.objects.filter(location=superloc).filter(name__unaccent__iexact=org.name).exclude(pk=org.pk):
+                for simorg in Organisation.objects.filter(location=loc).filter(name__unaccent__iexact=org.name).exclude(pk=org.pk):
                     print("\t", org, org.id, "is very similar to", simorg, simorg.id)
                     mergeorgs.append(simorg)
 
