7792998f34f64f744ad47f5525d19cc1c20fce46
Tom Ruette <tom.ruette@gmail.com>
2021-03-22 18:51:14 +0100

    reduce end date of fb events in batch

--- hlwtadmin/management/commands/refresh_facebook_cas.py
+++ hlwtadmin/management/commands/refresh_facebook_cas.py
@@ -7,7 +7,7 @@ from requests import get
 from json import loads, dumps
 
 from dateparser import parse as dateparse
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import time
 from fake_useragent import UserAgent
@@ -24,8 +24,8 @@ class Command(BaseCommand):
         self.platform = GigFinder.objects.filter(name="www.facebook.com").first()
         self.ua = UserAgent()
 
-        # loop through all announcements of songkick that are not ignored
-        for concertannouncement in ConcertAnnouncement.objects.filter(gigfinder__name="www.facebook.com"):
+        # loop through all announcements of facebook that are not ignored
+        for concertannouncement in ConcertAnnouncement.objects.filter(gigfinder__name="www.facebook.com").filter(until_date__isnull=False):
 
             print("working on", concertannouncement, concertannouncement.pk)
             time.sleep(3)
@@ -49,45 +49,59 @@ class Command(BaseCommand):
                 print("ca geupdated", concertannouncement, concertannouncement.pk)
                 concertannouncement.save(update_fields=["description", "latitude", "longitude", "title", "date", "until_date", "last_seen_on"])
 
-    def get_event(self, event_id, test_file=None, test=False):
-        url = "http://mobile.facebook.com/events/" + str(event_id)
-        print(url)
+    def get_html(self, url):
+        time.sleep(5.0)
         headers = {'user-agent': self.ua.firefox}
-        r = """<html></html>"""
+        try:
+            r = get(url, headers=headers)
+            return r
+        except Exception as e:
+            print("exception", e)
+            self.get_html(url)
+
+    def get_event(self, event_id, test_file=None, test=False):
+        url = "https://mobile.facebook.com/events/" + str(event_id)
+        print(url)
+        r = None
         if not test:
-            time.sleep(5.0)
-            try:
-                r = get(url, headers=headers).text
-            except Exception as e:
-                self._get_event(event_id, test_file, test)
+            r = self.get_html(url)
         else:
             with open(test_file, "r", "utf-8") as f:
                 r = f.read()
-        soup = BeautifulSoup(r, 'html.parser')
-        try:
-            ld = loads(soup.find("script", {"type": "application/ld+json"}).text)
-            datum = dateparse(ld["startDate"]).date()
-            einddatum = dateparse(ld["endDate"]).date() if "endDate" in ld else None
-            location = self._get_location(ld)
-            titel = ld["name"]
-            desc = ld["description"]
-            event_data = {
-                "event_id": event_id,
-                "datum": datum,
-                "einddatum": einddatum,
-                "land": location["country"],
-                "stad": location["city"],
-                "venue": location["venue"],
-                "latitude": location["lat"],
-                "longitude": location["lng"],
-                "titel": titel[0:199],
-                "description": desc
-            }
-            print("event", event_data)
-        except AttributeError as e:
-            print("error", e)
-            event_data = {}
-        return event_data
+        if r:
+            soup = BeautifulSoup(r.text, 'html.parser')
+            try:
+                ld = loads(soup.find("script", {"type": "application/ld+json"}).text)
+                print("raw data", ld)
+                datum = dateparse(ld["startDate"])
+                einddatum = dateparse(ld["endDate"]) if "endDate" in ld else None
+                if einddatum:
+                    if einddatum.hour < 12 and datum < einddatum:
+                        einddatum = (einddatum - timedelta(days=1))
+                    einddatum = einddatum.date()
+                    if einddatum == datum.date():
+                        einddatum = None
+                datum = datum.date()
+                location = self._get_location(ld)
+                titel = ld["name"]
+                desc = ld["description"]
+                event_data = {
+                    "event_id": event_id,
+                    "datum": datum,
+                    "einddatum": einddatum,
+                    "land": location["country"],
+                    "stad": location["city"],
+                    "venue": location["venue"],
+                    "latitude": location["lat"],
+                    "longitude": location["lng"],
+                    "titel": titel[0:199],
+                    "description": desc
+                }
+                print("event", event_data)
+            except AttributeError as e:
+                print("error", e)
+                event_data = {}
+            return event_data
 
     def _get_location(self, ld):
         try:
@@ -106,7 +120,7 @@ class Command(BaseCommand):
             "lat": None,
             "lng": None,
         }
-        loc_info["venue"] = location_name[0:199]
-        loc_info["city"] = location_street[0:199]
-        loc_info["country"] = location_country[0:199]
+        loc_info["venue"] = location_name[0:199] if location_name is not None else ""
+        loc_info["city"] = location_street[0:199] if location_street is not None else ""
+        loc_info["country"] = location_country[0:199] if location_country is not None else ""
         return loc_info
