ce6b255be86bed4bac2f2f0f22e6374ec81d921f
Tom Ruette <tom.ruette@gmail.com>
2021-03-28 12:10:35 +0200

    let's start on the megamerge for vague concerts

--- hlwtadmin/management/commands/merge_more_vaguely_dated_counterparts_of_existing_concerts.py
+++ hlwtadmin/management/commands/merge_more_vaguely_dated_counterparts_of_existing_concerts.py
@@ -13,62 +13,12 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        # get all concerts from all organisations
-        concerts = Concert.objects.all(
-                    ).prefetch_related(
-                        Prefetch('organisation', Organisation.objects.all(), to_attr='related_organisations'),
-                        Prefetch('artist', Artist.objects.all(), to_attr='related_artists')
-                        )#.distinct() ?
-
-        # check whether single date exists?
-        progress = 0
-        total = len(concerts)
-        print(f'Total concerts: {total}')
+        # get concerts from all organisations with vague dates
+        concerts = Concert.objects.filter(until_date__isnull=False)
         for concert in concerts:
-            progress += 1
-            if progress % 1000 == 0:
-                print(f'{progress}/{total}')
-            checked_tuples = []
-            for artist in concert.related_artists:
-                for organisation in concert.related_organisations:
-                        possible_duplicates = Concert.objects.filter(artist=artist, organisation=organisation)
-                        for candidate in possible_duplicates:
-                            duplicates = possible_duplicates.filter(title=candidate.title)
-                            if len(duplicates) > 1:
-                                specifics = []
-                                longer = []
-                                for d in duplicates:
-                                    if d.until_date:
-                                        if d.until_date != d.date:
-                                            longer.append(d)
-                                        else:
-                                            specifics.append(d)
-                                    else:
-                                        specifics.append(d)
-                                if longer and specifics:
-                                    for l in longer:
-                                        for s in specifics:
-                                            if l.date <= s.date <= l.until_date:
-                                                try:
-                                                    mm = MergedModelInstance.create(s, l)
-                                                    mm.date = concert_target.date
-                                                    mm.until_date = concert_target.until_date
-                                                    mm.save()
-                                                    #concert_merge = ConcertsMerge.objects.create(primary_object=s)
-                                                    #concert_merge.alias_objects.set(l)
-                                                    #concert_merge.merge()
-                                                    #concert_merge.delete()
-                                                except:
-                                                    # already deleted
-                                                    continue
-                                '''
-                                # logic to merge duplicate single concerts, Tom wrote something that is perhaps safer, or at least more restricted
-                                existing_dates = []
-                                if len(specifics) > 1:
-                                    for s in specifics:
-                                        if s.date in existing_dates:
-                                            print('possible doubles?')
-                                            print(s)
-                                        else:
-                                            existing_dates.append(s.date)
-                                '''
+            print("looking for specific concert for", concert, concert.until_date)
+            similar_concerts = Concert.objects.filter(relationconcertartist__artist__in=[rel.artist for rel in concert.artistsqs()]).filter(relationconcertorganisation__organisation__in=[rel.organisation for rel in concert.organisationsqs()]).filter(until_date__isnull=True).filter(date__gte=concert.date).filter(date__lte=concert.until_date)
+            for similar_concert in similar_concerts:
+                print("\tsimilar", similar_concert)
+            input()
+
