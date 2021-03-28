40623383ae281450a84b18b396c184561299d88e
Tom Ruette <tom.ruette@gmail.com>
2021-03-28 15:58:11 +0200

    merge vague dates into specific ones

--- hlwtadmin/management/commands/merge_more_vaguely_dated_counterparts_of_existing_concerts.py
+++ hlwtadmin/management/commands/merge_more_vaguely_dated_counterparts_of_existing_concerts.py
@@ -1,4 +1,4 @@
-from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Location, Organisation, Country, Concert, RelationConcertConcert, RelationConcertOrganisation, RelationConcertArtist, Location
+from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Location, Organisation, Country, Concert, RelationConcertConcert, RelationConcertOrganisation, RelationConcertArtist, Location, ConcertsMerge
 
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Count
@@ -14,11 +14,32 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # get concerts from all organisations with vague dates
-        concerts = Concert.objects.filter(until_date__isnull=False).filter(artist__mbid="68ec1b08-3c2e-40b3-b0af-8f323743fdff")
+        concerts = Concert.objects.filter(until_date__isnull=False)
         for concert in concerts:
             print("looking for specific concert for", concert.pk, concert, concert.until_date)
             similar_concerts = Concert.objects.filter(relationconcertartist__artist__in=[rel.artist for rel in concert.artistsqs()]).filter(relationconcertorganisation__organisation__location__in=[rel.organisation.location for rel in concert.organisationsqs()]).filter(until_date__isnull=True).filter(date__gte=concert.date).filter(date__lte=concert.until_date)
-            for similar_concert in similar_concerts:
-                print("\tsimilar", similar_concert)
+            if similar_concerts:
+                similar_concert = similar_concerts[0]
+                print("\tsimilar", similar_concert.pk, similar_concert)
                 input()
+
+                mergeconcerts = [concert]
+
+                if len(mergeconcerts) > 0:
+                    print("\tlet's do this, merge", mergeconcerts, "into", similar_concert)
+                    try:
+                        concert_merge = ConcertsMerge.objects.create(
+                            primary_object=similar_concert
+                        )
+                        concert_merge.alias_objects.set(mergeconcerts)
+                        print(concert_merge)
+                        concert_merge.merge()
+                        print("\tdone")
+                        concert_merge.delete()
+                        similar_concert.until_date = None
+                        similar_concert.save(update_fields=["until_date"])
+                    except Exception as e:
+                        print("\texception", e)
+                        pass
+
 
