cca135560bf7406163f600570018f51ba15df316
Tom Ruette <tom.ruette@gmail.com>
2021-03-28 12:34:16 +0200

    let's start on the megamerge for vague concerts 3

--- hlwtadmin/management/commands/merge_more_vaguely_dated_counterparts_of_existing_concerts.py
+++ hlwtadmin/management/commands/merge_more_vaguely_dated_counterparts_of_existing_concerts.py
@@ -16,7 +16,7 @@ class Command(BaseCommand):
         # get concerts from all organisations with vague dates
         concerts = Concert.objects.filter(until_date__isnull=False).filter(artist__mbid="68ec1b08-3c2e-40b3-b0af-8f323743fdff")
         for concert in concerts:
-            print("looking for specific concert for", concert, concert.until_date)
+            print("looking for specific concert for", concert.pk, concert, concert.until_date)
             similar_concerts = Concert.objects.filter(relationconcertartist__artist__in=[rel.artist for rel in concert.artistsqs()]).filter(relationconcertorganisation__organisation__location__in=[rel.organisation.location for rel in concert.organisationsqs()]).filter(until_date__isnull=True).filter(date__gte=concert.date).filter(date__lte=concert.until_date)
             for similar_concert in similar_concerts:
                 print("\tsimilar", similar_concert)
