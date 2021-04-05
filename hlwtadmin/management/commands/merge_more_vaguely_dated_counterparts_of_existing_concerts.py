6cc7eb2ad544e393765f1384e5affa1e5667af4a
Tom Ruette <tom.ruette@gmail.com>
2021-04-05 09:44:15 +0200

    throw a slightly wider net to find concerts also in part of locations

--- hlwtadmin/management/commands/merge_more_vaguely_dated_counterparts_of_existing_concerts.py
+++ hlwtadmin/management/commands/merge_more_vaguely_dated_counterparts_of_existing_concerts.py
@@ -1,4 +1,4 @@
-from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Location, Organisation, Country, Concert, RelationConcertConcert, RelationConcertOrganisation, RelationConcertArtist, Location, ConcertsMerge
+from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Location, Organisation, Country, Concert, RelationConcertConcert, RelationConcertOrganisation, RelationConcertArtist, Location, ConcertsMerge, RelationLocationLocation
 
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Count
@@ -17,7 +17,14 @@ class Command(BaseCommand):
         concerts = Concert.objects.filter(until_date__isnull=False)
         for concert in concerts:
             print("looking for specific concert for", concert.pk, concert, concert.until_date)
-            similar_concerts = Concert.objects.filter(relationconcertartist__artist__in=[rel.artist for rel in concert.artistsqs()]).filter(relationconcertorganisation__organisation__location__in=[rel.organisation.location for rel in concert.organisationsqs()]).filter(until_date__isnull=True).filter(date__gte=concert.date).filter(date__lte=concert.until_date)
+            locations = []
+            for rel in concert.organisationsqs():
+                location = rel.organisation.location
+                if location:
+                    locations.extend([subloc[0] for subloc in RelationLocationLocation.objects.filter(location_b=location).filter(relation_type__pk=1).values_list('location_a')])  # id=1 > is part of
+                    locations.append(location.pk)
+            if len(locations) > 0:
+                similar_concerts = Concert.objects.filter(relationconcertartist__artist__in=[rel.artist for rel in concert.artistsqs()]).filter(relationconcertorganisation__organisation__location__pk__in=locations).filter(until_date__isnull=True).filter(date__gte=concert.date).filter(date__lte=concert.until_date)
             if similar_concerts:
                 similar_concert = similar_concerts[0]
                 print("\tsimilar", similar_concert.pk, similar_concert)
