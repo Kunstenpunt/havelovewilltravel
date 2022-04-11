17898673b5438c8ddd6d8187eed5a74520049e05
Tom Ruette <tom.ruette@gmail.com>
2022-04-11 11:39:00 +0200

    adding a way to clean up concertannouncement history

---
+++ hlwtadmin/management/commands/clean_wild_concertannouncement_history.py
@@ -0,0 +1,13 @@
+from hlwtadmin.models import Artist, GigFinderUrl, GigFinder, ConcertAnnouncement, Venue, Location, Organisation, Country, Concert, RelationConcertConcert, RelationConcertOrganisation, RelationConcertArtist, Location, LocationsMerge
+from datetime import timedelta
+from django.utils import timezone
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        ConcertAnnouncement.history.filter(history_date__lt=timezone.now() - timedelta(days=15)).delete()
+
